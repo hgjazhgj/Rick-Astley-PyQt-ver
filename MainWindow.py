7331e5a85386c18e85aea0ad142606688bbc8552
hgjazhgj <huguangjing0411@geektip.cc>
2022-04-04 23:16:42 +0800

    update window title (issue #2)

--- MainWindow.py
+++ MainWindow.py
@@ -418,7 +418,7 @@ class Ui_MainWindow(object):
 
     def retranslateUi(self, MainWindow):
         _translate = QtCore.QCoreApplication.translate
-        MainWindow.setWindowTitle(_translate("MainWindow", "You are cheated!"))
+        MainWindow.setWindowTitle(_translate("MainWindow", "Get Rick Rolled lol"))
         self.label.setText(_translate("MainWindow", "We are"))
         self.CBB_1.setText(_translate("MainWindow", "Yes"))
         self.label_2.setText(_translate("MainWindow", "strangers"))
